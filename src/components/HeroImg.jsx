7ab77a7185876ba9b807343f0946e801588dd42b
Bekzat-frontend <mamateminovbekzat793@gmail.com>
2025-02-16 04:22:41 +0700

    change styless

--- src/components/HeroImg.jsx
+++ src/components/HeroImg.jsx
@@ -8,17 +8,21 @@ function HeroImg() {
       <div className="mask">
         <img
           className="into-img"
-          src="https://images.pexels.com/photos/378570/pexels-photo-378570.jpeg"
+          src="https://img.freepik.com/free-vector/abstract-black-texture-background-hexagon_206725-413.jpg"
           alt="IntroImg"
         />
       </div>
 
       <div className="content">
-        <p> HI,I'M A BEKZAT</p>
+        <p> HI, I'M A BEKZAT</p>
         <h1>React Developer</h1>
         <div>
-          <Link to="/project" className="btn">Project</Link>
-          <Link to="/contact" className="btn btn-light">CONTACT</Link>
+          <Link to="/project" className="btn">
+            Project
+          </Link>
+          <Link to="/contact" className="btn btn-light">
+            CONTACT
+          </Link>
         </div>
       </div>
     </div>
