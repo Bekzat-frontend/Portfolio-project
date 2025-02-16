351da220d9ebcf9cadb0c96184c195cd5a9ac8d2
Bekzat-frontend <mamateminovbekzat793@gmail.com>
2025-02-16 17:41:06 +0700

    craete cardes

--- src/routes/Project.jsx
+++ src/routes/Project.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import HerolImg2 from "../components/HerolImg2";
 import Footer from "../components/Footer";
+import PricingCard from "../components/PricingCard";
 
 function Project() {
   return (
     <div>
       <Navbar />
       <HerolImg2 heading="PROJECT." text="Some of my most recent works" />
+      <PricingCard />
       <Footer />
     </div>
   );
